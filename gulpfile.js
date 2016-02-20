393c6646186806ec289e61e0b010167e738b1321
Rafal Sladek <rafal.sladek@gmail.com>
2016-02-20 15:15:37 +0100

    added sass task and scss file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var del = require('del');
 
 
-gulp.task('copy', function(){
-	return gulp.src('src/assets/**')
-		.pipe(gulp.dest('out/'));
-});
+gulp.task('default', ['build']);
 
-gulp.task('clean',  function(){
-	return del('out/');
-});
+gulp.task('build', ['copy', 'sass']);
+
+gulp.task('sass', copyScssTask);
+
+gulp.task('copy', copyTask);
+
+gulp.task('clean', cleanUp);
+
+function copyTask() {
+    return gulp.src('src/assets/**')
+        .pipe(gulp.dest('out/'));
+}
+
+function copyScssTask() {
+    return gulp.src('src/styles/**')
+        .pipe(sass())
+        .pipe(gulp.dest('out/'));
+}
+
+function cleanUp(){
+    return del('out/');
+}
