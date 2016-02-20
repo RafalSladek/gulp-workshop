bad32b0077ab0df0634e562726c4659e7168a786
Rafal Sladek <rafal.sladek@gmail.com>
2016-02-20 14:54:20 +0100

    added clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
+var del = require('del');
 
 
 gulp.task('copy', function(){
 	return gulp.src('src/assets/**')
 		.pipe(gulp.dest('out/'));
+});
+
+gulp.task('clean',  function(){
+	return del('out/');
 });
