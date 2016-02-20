897a6e43c22a36936b4a1f671b7d7484a08951e6
Rafal Sladek <rafal.sladek@gmail.com>
2016-02-20 16:11:16 +0100

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,28 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var del = require('del');
 
-
 gulp.task('default', ['build']);
 
 gulp.task('build', ['copy', 'sass']);
+
+gulp.task('watch', ['watch:scss', 'watch:copy']);
+gulp.task('watch:scss', watchScssTask);
+gulp.task('watch:copy', watchCopyTask);
 
 gulp.task('sass', copyScssTask);
 
 gulp.task('copy', copyTask);
 
 gulp.task('clean', cleanUp);
+
+
+function watchScssTask(){
+    gulp.watch('src/styles/*.scss', {}, ['scss'])
+}
+
+function watchCopyTask(){
+    gulp.watch('src/assets/**', {}, ['copy'])
+}
 
 function copyTask() {
     return gulp.src('src/assets/**')
